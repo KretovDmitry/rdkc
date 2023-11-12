be078a6b5e7ba007b6b4b9ffa69fcd5728e4c968
Kretov Dmitry <Kretov_1996@bk.ru>
2023-11-12 13:22:06 +0300

    Delete client directory

--- client/src/App.js
+++
@@ -1,9 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import React from "react";
-import { router } from "./routes";
-
-function App() {
-  return <RouterProvider router={router} />;
-}
-
-export default App;
